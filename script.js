a7c375d1dd9570363c87ef9d9ec8a263e4f38ee5
Nneif <nneifemakpunonu@gmail.com>
2023-04-01 18:30:07 -0400

    added the values possible in the password generator and combined them into one variable

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 // Assignment code here
+var alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
+var captialAlapbets = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+var numerics = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+var specialCharacters = [' ', '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '>', '=', '?', '@', '[', ' \ ', ']', '^', '_', '`', '{', '}', '|', '~'];
+
+var passwordSet = alphabets + captialAlapbets + numerics + specialCharacters;
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
