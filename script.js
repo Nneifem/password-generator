e13a941f2b4fb358dbe8c693b3c06a40fa0af15d
Nneif <nneifemakpunonu@gmail.com>
2023-04-04 22:04:50 -0400

    added conditional statements to make the password include or not include certain attributes to the password

--- script.js
+++ script.js
@@ -4,8 +4,15 @@
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword(){
-  var passwordOptions = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ' ', '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '>', '=', '?', '@', '[', ' \ ', ']', '^', '_', '`', '{', '}', '|', '~'];
+  var lowercaseLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']; 
+  
+  var uppercaseLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+  
+  var numerics = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']; 
+  
+  var specialCharacters = [' ', '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '>', '=', '?', '@', '[', ' \ ', ']', '^', '_', '`', '{', '}', '|', '~'];
 
+  var passwordOptions = [];
   var finalPassword = [];
   
   var passwordLength = parseInt(prompt("How long do you want your password to be? Must be 8 to 128 characters long."));
@@ -13,20 +20,31 @@ function generatePassword(){
       prompt("try again, enter another length between 8 and 128");
     }
 
-    var question1 = prompt("Do you want lowercase letters in your password? Click okay for yes or cancel for no.");
+    var question1 = confirm("Do you want lowercase letters in your password? Click okay for yes or cancel for no.");
+      if(question1 === true){
+        passwordOptions = passwordOptions.concat(lowercaseLetters);
+      }
       
-    var queston2 = prompt("Do you want uppercase letters in your password? Click okay for yes or cancel for no.");
+    var question2 = confirm("Do you want uppercase letters in your password? Click okay for yes or cancel for no.");
+      if(question2 === true){
+        passwordOptions = passwordOptions.concat(uppercaseLetters);
+      }
       
-    var question3 = prompt("Do you want numbers in your password? Click okay for yes or cancel for no.");
+    var question3 = confirm("Do you want numbers in your password? Click okay for yes or cancel for no.");
+      if(question3 === true){
+        passwordOptions = passwordOptions.concat(numerics);
+      }
       
-    var question4 = prompt("Do you want special characters in your password? Click okay for yes or cancel for no.");
-    
-
+    var question4 = confirm("Do you want special characters in your password? Click okay for yes or cancel for no.");
+      if(question4 === true){
+        passwordOptions = passwordOptions.concat(specialCharacters);
+      }
+      console.log(passwordOptions);
     for(var i = 0; i < passwordLength; i++){
       finalPassword.push(passwordOptions[Math.floor(Math.random() * passwordOptions.length)]);
     }
     
-    console.log(finalPassword);
+    console.log(passwordOptions);
     return finalPassword.join("");
 
 }
